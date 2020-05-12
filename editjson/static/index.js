4f3c400fa5f8fb8d62523515cb8648581a63b4d4
Jered McInerney <jered.mcinerney@gmail.com>
2020-05-12 18:00:12 -0400

    restructured more

--- editjson/static/index.js
+++ editjson/static/index.js
@@ -1,5 +1,27 @@
 var json_viewer = null;
 var selected_param = null;
+class GlobalState {
+    constructor() {
+        this.max_global_id = 0;
+        this.global_id_holes = new Set([]);
+    }
+    removeId(id) {
+        id = id.slice("global_state_".length);
+        this.global_id_holes.add(id);
+    }
+    getFreshId() {
+        if (this.global_id_holes.size > 0) {
+            var id = this.global_id_holes.values().next().value;
+            this.global_id_holes.delete(id);
+        } else {
+            var id = this.max_global_id;
+            this.max_global_id += 1;
+        }
+        return "global_state_"+id;
+    }
+}
+var global_state = new GlobalState();
+
 $(document).ready(function(){
     onLoad();
 });
@@ -12,8 +34,6 @@ function onLoad() {
     } else {
         console.log("no json uploaded")
     }
-    d3.select("#exit").on("click", exitDetails);
-    d3.select("#param-type").on("change", onSelectType);
 }
 
 // taken from https://stackoverflow.com/questions/36127648/uploading-a-json-file-and-using-it
@@ -52,9 +72,12 @@ function showJSON(json_string, title="Default JSON") {
 }
 
 class JSONViewer {
-    constructor(json, prefix=[]) {
+    constructor(json, parent=null, name="top") {
         this.json = json;
-        this.prefix = prefix;
+        this.parent = parent;
+        this.name = name;
+        this.parent_div = null; // set when display is called
+        this.container = null; // set when display is called
         this.recurse();
     }
     recurse() {
@@ -62,11 +85,22 @@ class JSONViewer {
     display(parent_div) {
         this.parent_div = parent_div.html("");
     }
+    rename(newname) {
+        this.name = newname;
+    }
+    prefix() {
+        var prefix = [this.name];
+        if (this.parent != null) {
+            prefix = this.parent.prefix().concat(prefix);
+        }
+        return prefix;
+    }
 }
 
 class JSONNonLeaf extends JSONViewer {
     recurse() {
-        this.viewers = {};
+        this.viewers = {}; // stores all sub views
+        this.elements = {}; // stores any elements that need to be referenced
     }
     display(parent_div){
         super.display(parent_div);
@@ -77,12 +111,38 @@ class JSONNonLeaf extends JSONViewer {
             this.showTabView();
         }
     }
+    // function isTable(o, checkleaf=false) {
+    //     rows = Object.keys(o);
+    //     if (rows.length <= 0) {
+    //         return false;
+    //     }
+    //     if (isLeaf(Object.values(o)[0])) {
+    //         return false;
+    //     }
+    //     columns = Object.keys(Object.values(o)[0]);
+    //     columns_set = new Set(columns);
+    //     for (i = 0; i < rows.length; i++) {
+    //         row_columns = Object.keys(o[rows[i]]);
+    //         if (row_columns.length != columns.length) {
+    //             return false;
+    //         }
+    //         for (j = 0; j < row_columns.length; j++) {
+    //             if (!columns_set.has(row_columns[j])) {
+    //                 return false;
+    //             }
+    //             if (checkleaf && !isLeaf(o[rows[i]][row_columns[j]])) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
     isTable(){
-        return false;
+        return false; //TODO: implement this
     }
     showTabView(){
-        this.tabview = true;
         var temp_this = this;
+        this.tabview = true;
         if (this.isTable()) {
             this.container
               .html("")
@@ -94,8 +154,7 @@ class JSONNonLeaf extends JSONViewer {
         }
         var kv_pair_div = this.container
           .append("div")
-          .attr("id", "kvlist")
-          .attr("class", "container grid-container")
+          .attr("class", "container grid-container kvlist")
           .selectAll("div")
           .data(Object.keys(this.viewers).filter(function(e){ return temp_this.viewers[e] instanceof Param; }))
           .enter()
@@ -103,43 +162,47 @@ class JSONNonLeaf extends JSONViewer {
             .attr("class", "row");
         kv_pair_div
           .append("div")
-          .attr("id", function(d){ return d; })
           .attr("class", "col-sm")
           .html(function(d){ return d; });
         kv_pair_div
           .append("div")
-          .attr("id", function(d){ return d+"_value"; })
           .attr("class", "col-sm keys")
           .each(function(d){ temp_this.viewers[d].display(d3.select(this)); });
 
         var tabs = Object.keys(this.viewers).filter(function(e){ return temp_this.viewers[e] instanceof JSONNonLeaf; });
+        var ids = {};
+        tabs.forEach(function(t){
+            ids[t] = [global_state.getFreshId(), global_state.getFreshId()];
+        });
 
         var tabitems = this.container
           .append("ul")
-          .attr("id", "_"+replace_sep_tokens(temp_this.prefix.join("_")))
           .attr("class", "nav nav-tabs")
           .selectAll("li")
           .data(tabs)
           .enter()
             .append("li")
+            .attr("id", function(d){ return ids[d][0]; })
             .attr("class", "nav-item")
+            .each(function(d){ temp_this.elements[d+"_tab"] = this; })
             .append("a")
             .attr("class", "nav-link")
             .attr("data-toggle", "tab")
-            .attr("href", function(d){ return "#_"+replace_sep_tokens(temp_this.prefix.join("_")+"__"+d); })
-            .html(function(d){ return d; })
+            .attr("href", function(d){ return "#"+ids[d][1]; })
+            .html(function(d){ return d; });
 
         var tab_content_wrapper = this.container
           .append("div")
           .attr("class", "tab-content")
-        tabs.forEach(function(e){
-            var tab_content = tab_content_wrapper.append("div")
-              .attr("id", "_"+replace_sep_tokens(temp_this.prefix.join("_")+"__"+e))
-              .attr("class", "tab-pane fade");
-            temp_this.viewers[e].display(tab_content);
-        });
+          .selectAll("div")
+          .data(tabs)
+          .enter()
+            .append("div")
+            .attr("id", function(d){ return ids[d][1]; })
+            .attr("class", "tab-pane fade")
+            .each(function(d){ temp_this.viewers[d].display(d3.select(this)); });
     }
-    showGridView(){
+    showGridView() {
         this.tabview = false;
         var temp_this = this;
         if (this.isTable()) {
@@ -154,7 +217,8 @@ class JSONNonLeaf extends JSONViewer {
         var grid_container = this.container
           .append("div")
           .attr("class", "container grid-container")
-        var columns = Object.keys(Object.values(this.json)[0]);
+        var columns = Object.keys(Object.values(this.viewers)[0]);
+
         grid_container
           .append("div")
           .attr("class", "row")
@@ -163,7 +227,7 @@ class JSONNonLeaf extends JSONViewer {
           .enter()
             .append("div")
             .attr("class", "col-sm")
-            .html(function(d){ return d; })
+            .html(function(d){ return d; });
         Object.keys(this.viewers).forEach(function(e1){
             var row = grid_container
               .append("div")
@@ -189,111 +253,77 @@ class JSONDictionary extends JSONNonLeaf {
         var temp_this = this;
         Object.keys(this.json).forEach(function(k){
             var v = temp_this.json[k];
-            var new_prefix = temp_this.prefix.concat([k]);
             if (v == null) {
-                var viewer = null;
+                temp_this.viewers[k] = null;
             } else if (typeof v == "number") {
-                var viewer = new NumberParam(v, new_prefix);
+                temp_this.viewers[k] = new NumberParam(v, temp_this, k);
             } else if (typeof v == "string") {
-                var viewer = new StringParam(v, new_prefix);
+                temp_this.viewers[k] = new StringParam(v, temp_this, k);
             } else if (typeof v == "boolean") {
-                var viewer = new BooleanParam(v, new_prefix);
+                temp_this.viewers[k] = new BooleanParam(v, temp_this, k);
             } else if (Array.isArray(v)) {
-                var viewer = new JSONList(v, new_prefix);
+                temp_this.viewers[k] = new JSONList(v, temp_this, k);
             } else {
-                var viewer = new JSONDictionary(v, new_prefix);
+                temp_this.viewers[k] = new JSONDictionary(v, temp_this, k);
             }
-            temp_this.viewers[k] = viewer;
         });
     }
-// function isTable(o, checkleaf=false) {
-//     rows = Object.keys(o);
-//     if (rows.length <= 0) {
-//         return false;
-//     }
-//     if (isLeaf(Object.values(o)[0])) {
-//         return false;
-//     }
-//     columns = Object.keys(Object.values(o)[0]);
-//     columns_set = new Set(columns);
-//     for (i = 0; i < rows.length; i++) {
-//         row_columns = Object.keys(o[rows[i]]);
-//         if (row_columns.length != columns.length) {
-//             return false;
-//         }
-//         for (j = 0; j < row_columns.length; j++) {
-//             if (!columns_set.has(row_columns[j])) {
-//                 return false;
-//             }
-//             if (checkleaf && !isLeaf(o[rows[i]][row_columns[j]])) {
-//                 return false;
-//             }
-//         }
-//     }
-//     return true;
-// }
     showTabView() {
         super.showTabView();
         var temp_this = this;
-        d3.select("#_"+replace_sep_tokens(temp_this.prefix.join("_")))
-          .selectAll("a").on("dblclick", function(){ temp_this.changeKeyName(d3.select(this).html()); });
+        this.container
+          .select(".nav")
+          .selectAll(".nav-item")
+          .select("a")
+          .on("dblclick", function(){ temp_this.editKeyName(d3.select(this).html()); });
     }
-    changeKeyName(key) {
+    editKeyName(key) {
         var temp_this = this;
-        if (this.viewers[key] instanceof Param) { // is a key
-            if (this.tab_viewers != null) { // tab view
-                // tab = this.container.select("")
-
-                //     .classed("hide", true);
-                // var input = d3.select(item.node().parentNode)
-                //     .append("input")
-                //     .attr("class", "form-control")
-                //     .attr("placeholder", originalname);
-                // $(input.node()).on("click", function(){ return false; });
-                // $(document).on("click.forChangeTabName", function(){
-                //     temp_this.saveTabName(tabitem, input.node().value);
-                //     $(document).off("click.forChangeTabName");
-                // });
-                // //taken from https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
-                // // Execute a function when the user releases a key on the keyboard
-                // input.node().addEventListener("keyup", function(event) {
-                //     // Number 13 is the "Enter" key on the keyboard
-                //     if (event.keyCode === 13) {
-                //         // Cancel the default action, if needed
-                //         event.preventDefault();
-                //         // Trigger the button element with a click
-                //         temp_this.saveTabName(item, input.node().value);
-                //     }
-                // });
-            } else { // grid view
-            }
-        } else { // is not a key (only in grid view)
-            if (this.tab_viewers != null) {
-                // dont need to handle this case
-            } else { // grid view
-            }
+        if (this.tabview && (key in this.viewers) && (this.viewers[key] instanceof JSONNonLeaf)) {
+            var tab = d3.select(this.elements[key+"_tab"]);
+            tab.select("a").classed("hide", true);
+            var input = tab
+              .append("input")
+              .attr("class", "form-control")
+              .attr("placeholder", key);
+            $(input.node()).on("click", function(){ return false; });
+            $(document).on("click.forChangeKeyName", function(){
+                temp_this.changeKeyName(key, input.node().value);
+                $(document).off("click.forChangeKeyName");
+            });
+            //taken from https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+            // Execute a function when the user releases a key on the keyboard
+            input.node().addEventListener("keyup", function(event) {
+                // Number 13 is the "Enter" key on the keyboard
+                if (event.keyCode === 13) {
+                    // Cancel the default action, if needed
+                    event.preventDefault();
+                    // Trigger the button element with a click
+                    temp_this.changeKeyName(key, input.node().value);
+                }
+            });
+        } else { // may not need any other cases
         }
     }
-    saveKeyName(item, newname){
-        // var temp_this = this;
-        // item.parentNode().select("input").remove();
-        // var a = tabitem.select("a");
-        // var originalname = a.html();
-        // console.log(originalname);
-        // console.log(newname);
-        // a.classed("hide", false);
-        // if (newname.trim() == "" || originalname == newname) { return; }
-        // a
-        //   .attr("href", "#"+replace_sep_tokens(temp_this.prefix.join("_")+"__"+newname))
-        //   .html(newname);
-        // d3.select(tabitem.node().parentNode).select(replace_sep_tokens(temp_this.prefix.join("_")+"__"+originalname))
-        //   .attr("id", replace_sep_tokens(temp_this.prefix.join("_")+"__"+newname));
+    changeKeyName(key, newname){
+        var temp_this = this;
+        newname = newname.trim();
+        if (this.tabview && (key in this.viewers) && (this.viewers[key] instanceof JSONNonLeaf)) {
+            var tab = d3.select(this.elements[key+"_tab"]);
+            tab.select("input").remove();
+            var a = tab.select("a");
+            console.log(key);
+            console.log(newname);
+            a.classed("hide", false);
+            if (newname == "" || key == newname) { return; }
+            a.html(newname);
 
-        // //bookkeeping
-        // changeKey(this.tabs, originalname, newname);
-        // changeKey(this.json, originalname, newname);
-        // changeKey(this.tab_viewers, originalname, newname);
-        // this.tab_viewers[newname].prefix = this.prefix.concat([newname]);
+            //bookkeeping
+            changeKey(this.json, key, newname);
+            changeKey(this.viewers, key, newname);
+            this.viewers[newname].rename(newname);
+        } else { // may not need any other cases
+        }
     }
 }
 
@@ -317,7 +347,7 @@ class StringParam extends Param {
           .attr("type", "button")
           .attr("class", "btn btn-light")
           .html("...")
-          .on("click", function(){ openDetails(temp_this.prefix); });
+          .on("click", function(){ openDetails(temp_this.prefix()); });
         // change to a dropdown?
         // <div class="dropdown">
         //   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -340,14 +370,16 @@ class BooleanParam extends Param {
         var temp_this = this;
         var checkdiv = parent_div.append("div")
           .attr("class", "form-check");
+        var checkbox_id = global_state.getFreshId();
+        var checkbox_label_id = global_state.getFreshId();
         var checkbox = checkdiv
           .append("input")
           .attr("class", "form-check-input")
           .attr("type", "checkbox")
           .attr("value", this.json)
-          .attr("id", "_"+replace_sep_tokens(temp_this.prefix.join("_"))+"_checkbox")
+          .attr("id", checkbox_id)
           .on("click", function(){
-              var label = d3.select("#_"+replace_sep_tokens(temp_this.prefix.join("_"))+"_label");
+              var label = d3.select("#"+checkbox_label_id);
               label.html(!temp_this.json);
               temp_this.json = !temp_this.json;
           });
@@ -355,20 +387,16 @@ class BooleanParam extends Param {
         checkdiv
           .append("label")
           .attr("class", "form-check-label")
-          .attr("for", "_"+replace_sep_tokens(temp_this.prefix.join("_"))+"_checkbox")
-          .attr("id", "_"+replace_sep_tokens(temp_this.prefix.join("_"))+"_label")
+          .attr("for", checkbox_id)
+          .attr("id", checkbox_label_id)
           .html(this.json);
         checkdiv
           .append("button")
           .attr("type", "button")
           .attr("class", "btn btn-light")
           .html("...")
-          .on("click", function(){ openDetails(temp_this.prefix); });
+          .on("click", function(){ openDetails(temp_this.prefix()); });
     }
-}
-
-function replace_sep_tokens(s) {
-    return s.replace(/#/g, "_").replace(/ /g, "_").replace(/\//g, "_").replace(/=/g, "_").replace(/>/g, "_").replace(/</g, "_"); //.replace("\#", "_");
 }
 
 function changeKey(dictionary, originalname, newname){
@@ -376,8 +404,9 @@ function changeKey(dictionary, originalname, newname){
     delete dictionary[originalname];
 }
 
-function getViewerNode(viewer, prefix){
-    var curr = viewer;
+function getViewerNode(prefix){
+    prefix = prefix.slice(1);
+    var curr = json_viewer;
     for (var i = 0; i < prefix.length; i++) {
         curr = curr.viewers[prefix[i]];
     }
@@ -387,7 +416,7 @@ function getViewerNode(viewer, prefix){
 function openDetails(prefix) {
     selected_param = prefix;
     console.log(prefix);
-    var value = getViewerNode(json_viewer, prefix);
+    var value = getViewerNode(prefix);
     var param_details = d3.select("body").select("#param-details").classed("hide", false);
     param_details.select("#param-prefix")
       .html(prefix.join(" / "));
@@ -395,9 +424,9 @@ function openDetails(prefix) {
 }
 
 function onSelectType() {
-    var value = getViewerNode(json_viewer, selected_param);
+    var value = getViewerNode(selected_param);
     console.log(value);
-    var type = d3.select("#param-type").value;
+    var type = d3.select("#param-type").node().value;
     console.log(type);
     var param_details = d3.select("body").select("#param-details")
     if (type == "one-input") {
@@ -435,4 +464,3 @@ function exitDetails() {
     var selected_param = null;
     d3.select("body").select("#param-details").classed("hide", true);
 }
-
